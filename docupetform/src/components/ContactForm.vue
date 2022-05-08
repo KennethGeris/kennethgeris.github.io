fab39ae1a3abe3549a0b97e7fc1097f859463611
Kenneth <geriskenneth@gmail.com>
2022-05-08 12:29:57 -0400

    Add autocomplete, conditional rendering, form functionality

--- docupetform/src/components/ContactForm.vue
+++ docupetform/src/components/ContactForm.vue
@@ -18,10 +18,21 @@
 					</el-form-item>
 
 					<el-form-item label="Whats your pets's type?">
-						<el-radio-group v-model="form.type">
+						<el-radio-group v-model="form.type" @change="filterType">
 							<el-radio label="Cat" />
 							<el-radio label="Dog" />
 						</el-radio-group>
+					</el-form-item>
+
+					<el-form-item label="What breed are they?">
+						<el-autocomplete
+							v-model="form.breed"
+							:suffix-icon="Search"
+							:fetch-suggestions="querySearch"
+							:trigger-on-focus="true"
+							clearable
+							class="w-100"
+						/>
 					</el-form-item>
 
 					<el-row v-if="form.breed == `Can't find it?`">
@@ -69,7 +80,9 @@
 		</el-col>
 	</el-row>
 </template>
-<script setup></script>
+<script setup>
+import { Search } from "@element-plus/icons-vue";
+</script>
 <script>
 export default {
 	/* eslint-disable */
@@ -83,9 +96,23 @@ export default {
 			mix: "",
 			gender: "",
 		},
+		petTypeFilter: [],
 		petBreedList: [{ type: "Unknown", value: "Can't find it?" }],
 	}),
-
+	computed: {
+		isDisabled() {
+			if (
+				!this.form.name ||
+				!this.form.type ||
+				!this.form.breed ||
+				!this.form.gender ||
+				(this.form.breed == `Can't find it?` && !this.form.chooseAnswer) ||
+				(this.form.chooseAnswer == `It's a mix` && !this.form.mix)
+			) {
+				return true;
+			}
+		},
+	},
 	methods: {
 		// list of pets
 		loadPets() {
@@ -106,6 +133,42 @@ export default {
 				{ type: "Cat", value: "Sphynx" },
 			];
 		},
+
+		// filter list based on pet type
+		filterType(type, callback) {
+			this.petTypeFilter = [...this.petBreedList];
+			this.form.breed = "";
+
+			const results = this.petTypeFilter.filter(
+				this.createFilter(type, "type")
+			);
+
+			// keep first object and append results after
+			this.petTypeFilter = [...this.petTypeFilter.splice(0, 1), ...results];
+		},
+
+		// filter through pet type
+		querySearch(queryString, callback) {
+			this.form.chooseAnswer = "";
+			this.form.mix = "";
+
+			const results = queryString
+				? this.petTypeFilter.filter(this.createFilter(queryString, "value"))
+				: this.petTypeFilter;
+
+			//call callback function to return matching breeds
+			callback(results);
+		},
+
+		// return matching type and breeds
+		createFilter(queryString, property) {
+			return (breed) => {
+				return (
+					breed[property].toLowerCase().indexOf(queryString.toLowerCase()) === 0
+				);
+			};
+		},
+
 		// submit handler
 		onSubmit() {
 			console.log(this.form);
